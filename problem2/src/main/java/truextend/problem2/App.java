7b0fce17a6b2c70129f7f850d568e259daa6776f
jmpeace <jmpeace@gmail.com>
2020-03-03 11:41:04 -0400

    first commit

---
+++ problem2/src/main/java/truextend/problem2/App.java
@@ -0,0 +1,13 @@
+package truextend.problem2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
